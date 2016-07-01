39db4fed7b964776f0b598e92efb02de47a61640
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-07-01 09:44:56 -0500

    Initial Commit
    First commit of Old Maroon Rugby in .scss

--- index.php
+++ index.php
@@ -5,9 +5,135 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <link rel="stylesheet" href="css/style.css">
+        <link href="/css/style.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+        <script src="https://use.typekit.net/hcl1gto.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <div class="splash">
+            <div class="splash__container">
+                <div class="splash__header">
+                    <div class="splash__header-logo">
+                        <a href="/"><img src="/img/logo.png" class="img-fluid"></a>
+                    </div>
+                    <div class="splash__header-button">
+                        <a href="#"><span>Click for Navigation</span> <i class="fa fa-bars"></i></a>
+                    </div>
+                </div>
+
+                <div class="splash__copy">
+                    <div class="splash__copy-heading">
+                        <ul class="splash__copy-list">
+                            <li class="splash__copy-list-item">
+                                Tradition
+                            </li>
+                            <li class="splash__copy-list-item">
+                                <span class="splash__copy-star"><img src="/img/star.png"></span>
+                                Honor
+                                <span class="splash__copy-star"><img src="/img/star.png"></span>
+                            </li>
+                            <li class="splash__copy-list-item">
+                                Excellence
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="splash__copy-subheading">
+                        Old Maroon Rugby Football Club
+                    </div>
+                </div>
+
+                <div class="splash__social">
+                    <ul class="splash__social-links">
+                        <li class="splash__social-link">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li class="splash__social-link">
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li class="splash__social-link">
+                            <a href="#"><i class="fa fa-flickr"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="splash__border"></div>
+        </div>
+
+        <div class="home">
+            <div class="home__buttons">
+                <div class="home__button">
+                    <a class="home__button-link" href="#"><i class="fa fa-users"></i> Join</a>
+                </div>
+                <div class="home__button">
+                    <a class="home__button-link" href="#"><i class="fa fa-shopping-cart"></i> Merchandise</a>
+                </div>
+                <div class="home__button">
+                    <a class="home__button-link" href="#"><i class="fa fa-calendar"></i> Events</a>
+                </div>
+                <div class="home__button">
+                    <a class="home__button-link" href="#"><i class="fa fa-television"></i> Watch TAMU</a>
+                </div>
+            </div>
+            <div class="home__main">
+                <div class="home__content">
+                    <h1 class="home__content-heading">Texas A&M Old Maroon Rugby Football Club</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus, nibh nec ornare congue, mi leo dapibus erat, tempus pellentesque velit sapien vitae magna. Sed elit tellus, accumsan ut mollis et, facilisis sed turpis. Donec facilisis efficitur leo, ut iaculis turpis sagittis et. Nam ac lectus in orci fermentum feugiat ut non sapien. Vestibulum eget enim est. Suspendisse potenti. Mauris faucibus augue et pulvinar lobortis. Nunc tortor quam, molestie vitae enim nec, tempus vulputate nisi. Nullam risus velit, condimentum sit amet vestibulum vel, tempor nec felis. Suspendisse fringilla tincidunt pretium. Cras hendrerit faucibus tincidunt. Vivamus sit amet nunc sem.</p>
+
+                    <p>Duis eu gravida felis. Integer efficitur molestie orci, nec interdum mi. Sed ultrices nec sapien id porttitor. Suspendisse posuere vel dolor vitae tempus. Curabitur fringilla felis vitae eros finibus, id faucibus tellus auctor. Pellentesque varius eros eget erat aliquet, vel efficitur nisi tempus. Etiam eu aliquam magna. Nam feugiat purus a neque fringilla sodales. Pellentesque aliquam risus ut lectus aliquam, ut volutpat arcu tristique.</p>
+                </div>
+                <div class="home__star">
+                    <img src="/img/star-outline.png">
+                </div>
+                <div class="home__blog">
+                    <h3 class="home__blog-heading">Latest News</h3>
+                    <ul class="home__blog-list">
+                        <li class="home__blog-item">
+                            <a class="home__blog-link" href="#">Lorem ipsum dolor sit amet consectur adipiscing elit</a>
+                        </li>
+                    </ul>
+                    <p><a class="home__blog-button" href="#">See More <i class="fa fa-caret-right"></i></a></p>
+                </div>
+                <div class="home__photos">
+                    <img src="http://placehold.it/1600x900">
+                    <img src="http://placehold.it/1600x900">
+                </div>
+            </div>
+        </div>
+
+        <div class="footer__wrapper">
+            <div class="footer__border"></div>
+            <div class="container">
+                <div class="footer">
+                    <div class="footer__main">
+                        <ul class="footer__main-links">
+                            <li class="footer__main-link"><a href="#">Home</a></li>
+                            <li class="footer__main-link"><a href="#">Mission</a></li>
+                            <li class="footer__main-link"><a href="#">Officers</a></li>
+                            <li class="footer__main-link"><a href="#">Members</a></li>
+                            <li class="footer__main-link"><a href="#">Support the Ags</a></li>
+                            <li class="footer__main-link"><a href="#">Pay Your Dues</a></li>
+                            <li class="footer__main-link"><a href="#">Aggie Rugby</a></li>
+                            <li class="footer__main-link"><a href="#">Scholarships</a></li>
+                            <li class="footer__main-link"><a href="#">Photo Gallery</a></li>
+                            <li class="footer__main-link"><a href="#">Rugby Links</a></li>
+                            <li class="footer__main-link"><a href="#">Sponsors</a></li>
+                            <li class="footer__main-link"><a href="#">Contact</a></li>
+                            <li class="footer__main-link"><a href="#">Sitemap</a></li>
+                            <li class="footer__main-link"><a href="#">Login</a></li>
+                        </ul>
+
+                        <ul class="list-unstyled">
+                            <li>Copyright 2010-2016 Old Maroon Rugby Club. All rights reserved.</li>
+                            <li>Follow us on Facebook, Twitter, and Flickr</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
     </body>
 </html>
---
+++ inside.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <link href="/css/style.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+        <script src="https://use.typekit.net/hcl1gto.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    </head>
+    <body>
+        <div class="header__wrapper">
+            <div class="container">
+                <div class="header">
+                    <div class="header__logo">
+                    </div>
+                    <div class="header__social">
+                        <ul class="header__social-links">
+                            <li class="header__social-link">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li class="header__social-link">
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li class="header__social-link">
+                                <a href="#"><i class="fa fa-flickr"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="header__button">
+                        <a class="header__button-link" href="#"><span>Click for Navigation</span> <i class="fa fa-bars"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="content">
+                    <h1>Lorem Ipsum Dolor Sit Amet</h1>
+                    <p>Lorem ipsum dolot sit amet let's just start typing some words here the time has come for the working class to seize the means of production. We should forgive all medical debt and student loan debt. There should be a federal job guarentee. Post offices should be our banks.</p>
+
+                    <ul>
+                        <li>
+                            <a href="#">Lorem ipsum dolor sit amet consectur adipiscing elit</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="sidebar">
+                    <ul class="sidebar__buttons">
+                        <li class="sidebar__button">
+                            <a class="sidebar__button-link" href="#"><i class="fa fa-users"></i> Join</a>
+                        </li>
+                        <li class="sidebar__button">
+                            <a class="sidebar__button-link" href="#"><i class="fa fa-shopping-cart"></i> Merchandise</a>
+                        </li>
+                        <li class="sidebar__button">
+                            <a class="sidebar__button-link" href="#"><i class="fa fa-calendar"></i> Events</a>
+                        </li>
+                        <li class="sidebar__button">
+                            <a class="sidebar__button-link" href="#"><i class="fa fa-television"></i> Watch TAMU</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer__wrapper">
+            <div class="footer__border"></div>
+            <div class="container">
+                <div class="footer">
+                    <div class="footer__main">
+                        <ul class="footer__main-links">
+                            <li class="footer__main-link"><a href="#">Home</a></li>
+                            <li class="footer__main-link"><a href="#">Mission</a></li>
+                            <li class="footer__main-link"><a href="#">Officers</a></li>
+                            <li class="footer__main-link"><a href="#">Members</a></li>
+                            <li class="footer__main-link"><a href="#">Support the Ags</a></li>
+                            <li class="footer__main-link"><a href="#">Pay Your Dues</a></li>
+                            <li class="footer__main-link"><a href="#">Aggie Rugby</a></li>
+                            <li class="footer__main-link"><a href="#">Scholarships</a></li>
+                            <li class="footer__main-link"><a href="#">Photo Gallery</a></li>
+                            <li class="footer__main-link"><a href="#">Rugby Links</a></li>
+                            <li class="footer__main-link"><a href="#">Sponsors</a></li>
+                            <li class="footer__main-link"><a href="#">Contact</a></li>
+                            <li class="footer__main-link"><a href="#">Sitemap</a></li>
+                            <li class="footer__main-link"><a href="#">Login</a></li>
+                        </ul>
+
+                        <ul class="list-unstyled">
+                            <li>Copyright 2010-2016 Old Maroon Rugby Club. All rights reserved.</li>
+                            <li>Follow us on Facebook, Twitter, and Flickr</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
