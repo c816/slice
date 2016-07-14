b3ea270371dd78e28dd941fb86a7b855ca5790fa
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-07-14 07:39:40 -0500

    Adds Moor

--- index.php
+++ index.php
@@ -6,8 +6,166 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+        <script src="https://use.typekit.net/jnq5mbe.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <div class="splash">
+            <div class="header__wrapper">
+                <div class="container">
+                    <div class="header">
+                        <div class="header__logo">
+                            <img src="/img/header-logo.png">
+                        </div>
+                        <div class="header__phone">
+                            <a href="#"><i class="fa fa-phone"></i> 312-726-6207</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="overlay__wrapper">
+                <div class="container">
+                    <div class="overlay">
+                        <div class="overlay__text">
+                            <div class="overlay__text-heading">
+                                I represent ordinary people in an extraordinary way:
+                            </div>
+                            <div class="overlay__text-subheading">
+                                With Total Commitment.
+                            </div>
+                        </div>
+                        <div class="overlay__icon">
+                            <img src="/img/av.png">
+                        </div>
+                        <div class="overlay__icon">
+                            <a class="overlay__button" href="#">Email the Firm</a>
+                        </div>
+                        <div class="overlay__icon">
+                            <img src="/img/avvo.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="bio__wrapper">
+            <div class="container">
+                <div class="bio">
+                    <div class="bio__photo">
+                        <img src="http://placehold.it/480x480">
+                    </div>
+                    <div class="bio__content">
+                        <div class="bio__content-heading">
+                            Attorney Ed Moor
+                        </div>
+                        <div class="bio__content-body">
+                            Aliquam vulputate lacus sapien, eget vulputate urna pulvinar id. Mauris mi dolor, mollis vel eros nec, eleifend mollis leo. Ut tempus luctus ipsum. Aliquam erat volutpat. Pellentesque vitae leo magna. Nam vitae nibh lacinia, auctor dolor eget, gravida turpis. Aenean sit amet facilisis ligula. <a href="/about">Learn More ></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="home-content__wrapper">
+            <div class="container">
+                <div class="home-content">
+                    <div class="home-content__heading">
+                        <h1>Welcome to [firm-name]</h1>
+                    </div>
+                    <div class="home-content__body">
+                        <p>Facing an unexpected legal matter can be quite daunting for any individual. Most people do not have the time, knowledge, or ability to build a case on their own and instead wisely seek the help of an experienced attorney. An attorney can then handle all of the complicated aspects of the case on your behalf. Edward Moor of the Moor Law Office, P.C., is a trial lawyer who has experience handling a wide variety of cases that include:</p>
+                    </div>
+                    <ul class="home-content__list">
+                        <li class="home-content__list-item">
+                            Criminal Defense
+                        </li>
+                        <li class="home-content__list-item">
+                            Civil Rights
+                        </li>
+                        <li class="home-content__list-item">
+                            Qui Tam / Whistle Blower
+                        </li>
+                        <li class="home-content__list-item">
+                            Civil Litigation
+                        </li>
+                        <li class="home-content__list-item">
+                            Product Liability
+                        </li>
+                        <li class="home-content__list-item">
+                            Contract Disputes
+                        </li>
+                        <li class="home-content__list-item">
+                            Medical Malpractice
+                        </li>
+                        <li class="home-content__list-item">
+                            Personal Injury
+                        </li>
+                        <li class="home-content__list-item">
+                            Class Actions
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="home-quote__wrapper">
+            <div class="container">
+                <div class="home-quote">
+                    <div class="home-quote__body">
+                        <p>"Moore Law represented my business in litigation and has provided advice on a number of occasions. Clearly their experience proved invaluable. We prevailed in litigation and due to expert advice and excellent negotiation skills, avoided conflict in other transactions. I'd recommend Moore to anyone looking for competent, reliable legal service."</p>
+                    </div>
+                    <div class="home-quote__source">
+                        - Peter H. on Oct 3, 2015
+                    </div>
+                    <div class="home-quote__logo">
+                        <img src="/img/avvo.png">
+                    </div>
+                    <div class="home-quote__stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="home-map__wrapper">
+            <div class="home-map__inner">
+                <div class="container">
+                    <div class="home-map">
+                        <div class="home-map__overlay">
+                            <ul class="home-map__key">
+                                <li class="home-map__key-item --heading"><i class="fa fa-map"></i> Moor Law Office, P.C.</li>
+                                <li class="home-map__key-item">1 N. LaSalle Street</li>
+                                <li class="home-map__key-item">Suite 600</li>
+                                <li class="home-map__key-item">Chicago, IL 60602</li>
+                                <li class="home-map__key-item">312-726-6207</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer__wrapper">
+            <div class="container">
+                <div class="footer">
+                    <div class="footer__content">
+                        <ul class="list-unstyled">
+                            <li><a href="/">Home</a> &nbsp; | &nbsp; <a href="/about">About</a> &nbsp; | &nbsp; <a href="/contact">Contact</a></li>
+                            <li>&copy;Copyright 2016 - Moor Law Office, P.C. &nbsp; | &nbsp; Office: 312-726-6207 &nbsp; | &nbsp; Fax: 312-276-4457</li>
+                            <li>One North LaSalle Street, Suite 600</li>
+                            <li>Chicago, IL 60602</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
