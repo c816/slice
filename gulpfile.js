b7cf4ad40dfcc7df8afbde01a1bfa3c2aa5be917
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-07-14 07:22:41 -0500

    Adds Longhorn Imaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
+process.env.DISABLE_NOTIFIER = true;
+
 var elixir = require('laravel-elixir');
 
 elixir.config.appPath = '';
 elixir.config.assetsPath = '';
-elixir.config.notifications = false;
 elixir.config.publicPath = '';
 
 elixir(function(mix) {
--- index.php
+++ index.php
@@ -6,8 +6,175 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
         <link rel="stylesheet" href="css/style.css">
+
+        <script src="https://use.typekit.net/xhr2sxa.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <div class="Header__wrapper">
+            <div class="container">
+                <div class="Header">
+                    <div class="Header__logo">
+                        <img src="/img/logo.png">
+                    </div>
+                    <ul class="Header__list">
+                        <li class="Header__list-item --heading">Contact Us</li>
+                        <li class="Header__list-item --phone">512-444-8900</li>
+                        <li class="Header__list-item --subheading">South Austin &nbsp; | &nbsp; Central Austin &nbsp; | &nbsp; Cedar Park</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="Nav__wrapper">
+            <div class="container">
+                <div class="Nav">
+                    <ul class="Nav__list">
+                        <li class="Nav__list-item">
+                            <a class="Nav__link" href="#">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="MainGraphic__wrapper">
+            <div class="MainGraphic">
+                <img class="MainGraphic__overlay" src="/img/main-graphic-overlay.png">
+            </div>
+        </div>
+
+        <div class="Icons__wrapper">
+            <div class="container">
+                <div class="Icons">
+                    <div class="Icon">
+                        <img src="/img/icons/acr.png">
+                    </div>
+                    <div class="Icon">
+                        <img src="/img/icons/solutionreach.png">
+                    </div>
+                    <div class="Icon">
+                        <img src="/img/icons/care-credit.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeContent__wrapper">
+            <div class="container">
+                <div class="HomeContent">
+                    <div class="HomeContent__text">
+                        <h1>Welcome to Longhorn Imaging!</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim felis, euismod semper enim vel, feugiat interdum sem. Nam tempor ipsum ante, ut feugiat libero pulvinar quis. Aliquam mattis tellus est, in sagittis ex convallis nec. Aenean consectetur felis vitae libero tempor, ac fringilla lorem fringilla. Quisque laoreet urna eget eleifend maximus. Quisque laoreet dolor at lacus vulputate dignissim in sed urna. Fusce ac arcu porta, aliquet lacus quis, consequat mauris. Aliquam erat volutpat. Etiam et libero nec lorem tincidunt maximus eget et nibh. Curabitur sodales lobortis interdum. In hac habitasse platea dictumst. Sed interdum malesuada ipsum, sit amet fermentum tellus varius vitae. Ut luctus purus elit, nec finibus nunc pharetra ac. Aliquam nec porta justo, sed sollicitudin velit.</p>
+
+                        <p>Nunc neque diam, placerat et lectus vel, tempor eleifend tortor. Praesent justo tellus, laoreet sit amet nisl quis, egestas bibendum nibh. Etiam ornare dolor sed aliquam blandit. Curabitur sapien lorem, euismod eu lacus nec, faucibus gravida neque. Donec tristique dui tortor, et pretium tortor iaculis quis. Morbi porttitor sodales nisl, et sollicitudin tortor fermentum nec. Quisque eget libero ex. Quisque molestie urna sapien, non pellentesque metus auctor non. Duis pharetra, turpis quis pretium convallis, est orci posuere purus, ac bibendum augue turpis et mauris. Integer rutrum, est a condimentum tempor, felis sem hendrerit nulla, sit amet tincidunt metus diam quis magna.</p>
+                    </div>
+                    <div class="HomeContent__box">
+                        <a class="HomeContent__box-button" href="#">Preparing For Your MRI</a>
+                        <img class="HomeContent__box-image" src="http://placehold.it/500x400">
+                    </div>
+                    <div class="HomeContent__box">
+                        <a class="HomeContent__box-button" href="#">What to Expect</a>
+                        <img class="HomeContent__box-image" src="http://placehold.it/500x400">
+                    </div>
+                    <div class="HomeContent__box">
+                        <a class="HomeContent__box-button" href="#">FAQs</a>
+                        <img class="HomeContent__box-image" src="http://placehold.it/500x400">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeTestimonial__wrapper">
+            <div class="container">
+                <div class="HomeTestimonial">
+                    <div class="HomeTestimonial__quote">
+                        I just had the easiest MRI ever. I would def[initely] recommend Longhorn Imaging to my friends and family.
+                    </div>
+                    <div class="HomeTestimonial__source">
+                        - Jessica C.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="HomeLocations">
+                <div class="HomeLocations__header">
+                    Locations
+                </div>
+                <div class="HomeLocation">
+                    <div class="HomeLocation__map-wrapper">
+                        <iframe class="HomeLocation__map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13789.88643342721!2d-97.776104!3d30.223625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b4b715ca30dd%3A0x7f61ac90af961952!2sLonghorn+imaging!5e0!3m2!1sen!2sus!4v1468000677587" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    <a class="HomeLocation__button" href="#"><i class="fa fa-map-marker"></i> South Austin</a>
+                    <ul class="HomeLocation__list">
+                        <li class="HomeLocation__list-item">4316 James Casey</li>
+                        <li class="HomeLocation__list-item">Building F, Suite 110</li>
+                        <li class="HomeLocation__list-item">Austin, TX 78745</li>
+                        <li class="HomeLocation__list-item">Office: 512-444-8900</li>
+                    </ul>
+                </div>
+
+                <div class="HomeLocation">
+                    <div class="HomeLocation__map-wrapper">
+                        <iframe class="HomeLocation__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3445.827152251977!2d-97.7896895851953!3d30.270505714813517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865b4acafc13b2c9%3A0x4a20e9375a151079!2s2745+Bee+Cave+Rd+%23102%2C+Rollingwood%2C+TX+78746!5e0!3m2!1sen!2sus!4v1468000782045" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    <a class="HomeLocation__button" href="#"><i class="fa fa-map-marker"></i> Central Austin</a>
+                    <ul class="HomeLocation__list">
+                        <li class="HomeLocation__list-item">2745 Bee Cave Road</li>
+                        <li class="HomeLocation__list-item">Suite 102</li>
+                        <li class="HomeLocation__list-item">Rollingwood, TX 78746</li>
+                        <li class="HomeLocation__list-item">Office: 512-444-8900</li>
+                    </ul>
+                </div>
+
+                <div class="HomeLocation">
+                    <div class="HomeLocation__map-wrapper">
+                        <iframe class="HomeLocation__map" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d6874.091410983541!2d-97.83224257564676!3d30.5197466692705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s715+Discovery+Blvd+Suite+102!5e0!3m2!1sen!2sus!4v1468000892003" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    <a class="HomeLocation__button" href="#"><i class="fa fa-map-marker"></i> Cedar Park</a>
+                    <ul class="HomeLocation__list">
+                        <li class="HomeLocation__list-item">715 Discovery Blvd</li>
+                        <li class="HomeLocation__list-item">Suite 102</li>
+                        <li class="HomeLocation__list-item">Cedar Park, TX 78613</li>
+                        <li class="HomeLocation__list-item">Office: 512-444-8900</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="Footer__wrapper">
+            <div class="container">
+                <div class="Footer">
+                    <div class="Footer__logo">
+                        <img src="/img/logo-footer.png">
+                    </div>
+
+                    <ul class="Footer__list">
+                        <li class="Footer__list-item">Copyright &copy;2014-2016 Longhorn Imaging</li>
+                    </ul>
+
+                    <ul class="Footer__list">
+                        <li class="Footer__list-item">
+                            <a href="/">Home</a>
+                        </li>
+                        <li class="Footer__list-item">
+                            <a href="/about">About</a>
+                        </li>
+                        <li class="Footer__list-item">
+                            <a href="/services">Services</a>
+                        </li>
+                        <li class="Footer__list-item">
+                            <a href="#">Reading Radiologists</a>
+                        </li>
+                    </ul>
+
+                    <ul class="Footer__list">
+                        <li class="Footer__list-item">Main Office Line: 512-444-8900</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
