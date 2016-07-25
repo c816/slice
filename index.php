c16343865c8cfa3476cb5c89d127c1a01d862646
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-07-25 10:39:46 -0500

    Adds Therapy

--- index.php
+++ index.php
@@ -6,8 +6,301 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+        <script src="https://use.typekit.net/cgf5gps.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
     </head>
-    <body>
-        <h1>Hello, world!</h1>
+    <body class="body --home">
+        <div class="top-nav__wrapper">
+            <div class="top-nav">
+                <div class="top-nav__contact">
+                    <ul class="top-nav__list">
+                        <li class="top-nav__list-item">
+                            <i class="fa fa-map-marker"></i>
+                            <span class="top-nav__span">5371 Westheimer</span>
+                        </li>
+                        <li class="top-nav__list-item">
+                            <i class="fa fa-phone"></i>
+                            <span class="top-nav__span">(713) 355-HAIR</span>
+                        </li>
+                        <li class="top-nav__list-item">
+                            <i class="fa fa-calendar"></i>
+                            <span class="top-nav__span">Schedule Appointment</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="top-nav__social">
+                    <ul class="top-nav__list">
+                        <li class="top-nav__list-item">
+                            <i class="fa fa-facebook"></i>
+                        </li>
+                        <li class="top-nav__list-item">
+                            <i class="fa fa-instagram"></i>
+                        </li>
+                        <li class="top-nav__list-item">
+                            <i class="fa fa-snapchat"></i>
+                        </li>
+                        <li class="top-nav__list-item">
+                            <i class="fa fa-youtube-play"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="header__wrapper">
+            <div class="header">
+                <div class="header__logo">
+                    <img src="/img/logo.jpg">
+                </div>
+                <div class="header__nav">
+                    <ul class="header__list">
+                        <li class="header__list-item">
+                            <a class="header__link" href="#">About Us <i class="fa fa-caret-down"></i></a>
+                        </li>
+                        <li class="header__list-item">
+                            <a class="header__link" href="#">Stylists</i></a>
+                        </li>
+                        <li class="header__list-item">
+                            <a class="header__link" href="#">Services <i class="fa fa-caret-down"></i></a>
+                        </li>
+                        <li class="header__list-item">
+                            <a class="header__link" href="#">Galleries <i class="fa fa-caret-down"></i></a>
+                        </li>
+                        <li class="header__list-item">
+                            <a class="header__link" href="#">Blog</a>
+                        </li>
+                        <li class="header__list-item">
+                            <a class="header__link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="image-layer --main-graphic"></div>
+
+        <div class="home-box__wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="home-box">
+                        <div class="home-box__card">
+                            <div class="home-box__card-header">
+                                Therapy Hair Studio &ndash; A Houston Salon
+                            </div>
+                            <div class="home-box__card-stars">
+                                <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                            </div>
+                            <ul class="home-box__card-list">
+                                <li><i class="fa fa-yelp"></i> Yelp</li>
+                                <li><i class="fa fa-facebook"></i> Facebook</li>
+                                <li><i class="fa fa-google-plus"></i> Google+</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="home-content__wrapper --dark">
+            <div class="container">
+                <div class="home-content">
+                    <div class="home-content__header">
+                        <h1>TRANSFORMING THE HAIR SALON EXPERIENCE IN HOUSTON</h1>
+                    </div>
+                    <div class="home-content__paragraph">
+                        Getting professional hair care from one of the top salons in Houston shouldn't be a burden; it should be an experience, and a relaxing, enjoyable one at that. Every day at Therapy Hair Studio, master stylists David Bamford and Luis Perez work diligently to make sure you receive the precision cut, color and style that helps you look your absolute best.
+                    </div>
+                    <div class="home-content__paragraph">
+                        Enjoy champagne, wine, tea or a hot cappuccino while the professional stylists at Therapy Hair Studio work their creative magic on your hair. Let them treat you to a look that enhances your features and suits your personal style, using the finest hair salon products in Houston. Visit them once, and they're confident you'll come back again and again.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="image-layer" style="background-image: url('/img/layers/layer-1.jpg')"></div>
+
+        <div class="home-content__wrapper">
+            <div class="container">
+                <div class="home-content">
+                    <div class="home-content__header">
+                        <h2>The Therapy Hair Studio Team</h2>
+                    </div>
+                    <div class="home-content__stylist">
+                        <img class="home-content__stylist-photo" src="http://placehold.it/800x800">
+                        <div class="home-content__stylist-name">Luis Perez</div>
+                        <div class="home-content__stylist-title">Stylist Title</div>
+                    </div>
+                    <div class="home-content__stylist">
+                        <img class="home-content__stylist-photo" src="http://placehold.it/800x800">
+                        <div class="home-content__stylist-name">David Bamford</div>
+                        <div class="home-content__stylist-title">Stylist Title</div>
+                    </div>
+                    <div class="home-content__stylist">
+                        <img class="home-content__stylist-photo" src="http://placehold.it/800x800">
+                        <div class="home-content__stylist-name">Suleiman Telfah</div>
+                        <div class="home-content__stylist-title">Stylist Title</div>
+                    </div>
+                    <div class="home-content__stylist">
+                        <img class="home-content__stylist-photo" src="http://placehold.it/800x800">
+                        <div class="home-content__stylist-name">Anneliese Nicole</div>
+                        <div class="home-content__stylist-title">Stylist Title</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="image-layer" style="background-image: url('/img/layers/layer-2.jpg')"></div>
+
+        <div class="home-feature__wrapper">
+            <div class="container">
+                <div class="home-feature">
+                    <div class="home-feature__photo">
+                        <img src="http://placehold.it/800x450">
+                    </div>
+                    <div class="home-feature__content">
+                        <h3>Enhancing the Beauty in Each Client</h3>
+                        <p>Located in Houston’s prestigious Galleria area, Therapy Hair Studio specializes in bringing out the unique inner beauty of each client. The experienced staff offers a variety of services to meet your needs, elegant updos, naturally derived hair and scalp treatments and more. Since 2008, this top-notch hair salon has won the hearts of thousands of loyal customers throughout Houston and beyond. Let them do the same for you. Call 713.355.HAIR (4247) today to schedule an appointment.</p>
+                    </div>
+                </div>
+
+                <div class="home-feature --right">
+                    <div class="home-feature__photo">
+                        <img src="http://placehold.it/800x450">
+                    </div>
+                    <div class="home-feature__content">
+                        <h3>BRINGING HIGH GLAMOUR TO EVERYDAY LIVING</h3>
+                        <p>While there is no substitute for a precision cut and color from one of the top hair salons in Houston, Therapy Hair Studio prides itself on giving clients the tools they need to maintain their styles at home. Not only will you reap the benefits of flawless hair and makeup, but you will learn the necessary techniques to keep your hair looking flawless until your next visit.</p>
+                    </div>
+                </div>
+
+                <div class="home-feature">
+                    <div class="home-feature__photo">
+                        <img src="http://placehold.it/800x450">
+                    </div>
+                    <div class="home-feature__content">
+                        <h3>THE EXPERTS IN BRIDAL HAIR & MAKEUP</h3>
+                        <p>You've been dreaming about your wedding day ever since you were young, and the stylists at Therapy Hair Studio want to help make those dreams come true. Whether you're considering hair extensions for your big day or you need help from one of Houston's best makeup artists, Therapy Hair Studio has you covered. Your wedding day is too important for you to leave it to an amateur hair stylist. Choose the best hair salon in Houston, and you'll have one less thing to worry about when it's time to say, "I do."</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="image-layer" style="background-image: url('/img/layers/layer-3.jpg')"></div>
+
+        <div class="home-content__wrapper">
+            <div class="container">
+                <div class="home-content">
+                    <div class="home-content__header">
+                        <h2>We Proudly Serve</h2>
+                    </div>
+                    <div class="home-content__product">
+                        <img src="/img/brands/kevin-murphy.jpg">
+                    </div>
+                    <div class="home-content__product">
+                        <img src="/img/brands/kerastase.jpg">
+                    </div>
+                    <div class="home-content__product">
+                        <img src="/img/brands/oribe.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer__wrapper">
+            <div class="container">
+                <div class="footer">
+                    <div class="footer__map-wrapper">
+                        <div class="footer__map">
+                            <iframe class="footer__map-iframe" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13857.418268620277!2d-95.469471!3d29.738449999999997!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c165c9c5466d%3A0x757b8bf96163fbd7!2s5371+Westheimer+Rd%2C+Houston%2C+TX+77056!5e0!3m2!1sen!2sus!4v1468006042685" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div class="footer__text">
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">About Us</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Stylists</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Services</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Weddings</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Editorials</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Videos</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Galleries</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Blog</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Products</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Contact Us</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Site Map</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Login</a>
+                            </li>
+                        </ul>
+
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                &copy;2016, THERAPY HAIR STUDIO. ALL RIGHTS RESERVED.
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Best Hair Salon Houston</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Bridal Hair and Makeup Houston</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Houston Wedding Makeup Artist</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Best Hair Salon</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Wedding Makeup</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Bridal Hair</a>
+                            </li>
+                        </ul>
+
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                5371 Westheimer
+                            </li>
+                            <li class="list-inline-item">
+                                Houston, TX 77056
+                            </li>
+                            <li class="list-inline-item">
+                                713-355-HAIR (4247)
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Map and Directions</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
