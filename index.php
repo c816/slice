6d2e02e8ec6d8dd90bb11b912af9faef9564c78e
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-09-13 08:19:49 -0500

    Adds Truslow

--- index.php
+++ index.php
@@ -6,8 +6,300 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+        <script src="https://use.typekit.net/jkt5sbw.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <div class="Splash">
+            <div class="Nav__wrapper">
+                <div class="Nav">
+                    <div class="Nav__logo">
+                        <img src="/img/logo-sm.png">
+                    </div>
+
+                    <ul class="Nav__menu">
+                        <li class="Nav__menu-item">
+                            <a class="Nav__menu-link --active" href="#">Home</a>
+                        </li>
+                        <li class="Nav__menu-item">
+                            <a class="Nav__menu-link" href="#">About</a>
+                        </li>
+                        <li class="Nav__menu-item">
+                            <a class="Nav__menu-link" href="#">Practice Areas</a>
+                        </li>
+                        <li class="Nav__menu-item">
+                            <a class="Nav__menu-link" href="#">Expectations</a>
+                        </li>
+                        <li class="Nav__menu-item">
+                            <a class="Nav__menu-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+
+                    <ul class="Nav__contact">
+                        <li class="Nav__contact-tagline">Protect Your Rights!</li>
+                        <li class="Nav__contact-number"><i class="fa fa-phone text-primary"></i> (803) 256-6276</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="Splash__content">
+                <div class="row">
+                    <ul class="Splash__messaging">
+                        <li class="Splash__messaging-heading">
+                            <span>Effective <span class="text-primary">+</span> Efficient <span class="text-primary">+</span> Experienced</span>
+                        </li>
+                        <li>Your time is limited after a DUI.</li>
+                        <li class="Splash__messaging-strong">Don't waste it!</li>
+                        <li><a class="Button +contact" href="#">Free Case Review <i class="fa fa-caret-right"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="Splash__arrow">
+                <i class="fa fa-chevron-down"></i>
+            </div>
+        </div>
+
+        <div class="Stats__wrapper">
+            <div class="Stats">
+                <ul class="Stats__list">
+                    <li class="Stats__list-heading">
+                        500+
+                    </li>
+                    <li class="Stats__list-subheading">
+                        Cases Handled
+                    </li>
+                </ul>
+
+                <ul class="Stats__list">
+                    <li class="Stats__list-heading">
+                        30+
+                    </li>
+                    <li class="Stats__list-subheading">
+                        Years of Legal Experience
+                    </li>
+                </ul>
+
+                <ul class="Stats__list">
+                    <li class="Stats__list-heading">
+                        99%
+                    </li>
+                    <li class="Stats__list-subheading">
+                        Client Satisfaction
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="HomeContent">
+                <div class="HomeContent__first">
+                    <h1>Columbia DUI Lawyers</h1>
+                    <p>Vestibulum tristique et ex sed bibendum. Donec hendrerit ullamcorper sollicitudin. Nullam lacus leo, feugiat eget sapien eu, semper dignissim quam. Maecenas malesuada nulla ut lorem fermentum cursus. Praesent egestas sagittis magna, ac condimentum nunc imperdiet sed. Sed dapibus mauris sit amet egestas venenatis. Nulla faucibus dapibus nisi ut dictum. In consectetur quam in sodales tincidunt. Maecenas maximus tellus id est bibendum efficitur. Donec nec lorem aliquet nisi aliquet efficitur.</p>
+
+                    <p class="m-b-3">Proin varius enim libero. Quisque elit eros, commodo molestie arcu eget, placerat tincidunt mauris. Nulla massa lorem, rutrum at ligula ut, ornare sodales sapien. Sed vehicula, arcu et faucibus dapibus, nunc nulla vehicula mi, id auctor justo risus eu risus. In sollicitudin rutrum mauris, id rutrum sem fermentum eget. In metus velit, placerat sed tristique vel, fringilla vel ex. Aenean vitae lacus arcu. Nunc efficitur ac est quis pulvinar.</p>
+
+                    <h2>Our Attorneys</h2>
+                </div>
+            </div>
+            <div class="HomeAttorneys">
+                <div class="HomeAttorney">
+                    <div class="HomeAttorney__profile-wrapper --douglas">
+                        <div class="HomeAttorney__profile">
+                            <a class="HomeAttorney__profile-link" href="#">Douglas N. Truslow</a>
+                        </div>
+                    </div>
+
+                    <p>Praesent egestas sagittis magna, ac condimentum nunc imperdiet sed. Sed dapibus mauris sit amet egestas venenatis. Nulla faucibus dapibus nisi ut dictum. In consectetur quam in sodales tincidunt.</p>
+
+                    <a href="#">Read More+</a>
+                </div>
+
+                <div class="HomeAttorney">
+                    <div class="HomeAttorney__profile-wrapper --neal">
+                        <div class="HomeAttorney__profile">
+                            <a class="HomeAttorney__profile-link" href="#">Neal D. Truslow</a>
+                        </div>
+                    </div>
+
+                    <p>Praesent egestas sagittis magna, ac condimentum nunc imperdiet sed. Sed dapibus mauris sit amet egestas venenatis. Nulla faucibus dapibus nisi ut dictum. In consectetur quam in sodales tincidunt.</p>
+
+                    <a href="#">Read More+</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeTestimonials__wrapper">
+            <div class="container">
+                <div class="HomeTestimonials">
+                    <div class="HomeTestimonials__header">
+                        <h2>What Our Clients Are Saying</h2>
+                    </div>
+
+                    <ul class="HomeTestimonial">
+                        <li class="HomeTestimonial__quote">"I can't say enough about the job Truslow & Truslow did for me. I'm glad I chose them to help me with my legal issue."</li>
+                        <li class="HomeTestimonial__photo"><img src="/img/HomeTestimonial-photo.png"></li>
+                        <li class="HomeTestimonial__name">Name Withheld for Privacy</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="HomeContent">
+                <div class="HomeContent__questions-wrapper">
+                    <h2>Common DUI Questions</h2>
+                    <ul class="HomeContent__questions">
+                        <li class="HomeContent__questions-item">
+                            <a class="HomeContent__questions-link" href="#">What happens if I refuse a breathalyzer test?</a>
+                            In metus velit, placerat sed tristique vel, fringilla vel ex. Aenean vitae lacus arcu. Nunc efficitur ac est quis pulvinar. <a href="#">Read more</a>
+                        </li>
+                        <li class="HomeContent__questions-item">
+                            <a class="HomeContent__questions-link" href="#">What are the penalties for a first time DUI?</a>
+                            In metus velit, placerat sed tristique vel, fringilla vel ex. Aenean vitae lacus arcu. Nunc efficitur ac est quis pulvinar. <a href="#">Read more</a>
+                        </li>
+                        <li class="HomeContent__questions-item">
+                            <a class="HomeContent__questions-link" href="#">What happens to my license after a DUI?</a>
+                            In metus velit, placerat sed tristique vel, fringilla vel ex. Aenean vitae lacus arcu. Nunc efficitur ac est quis pulvinar. <a href="#">Read more</a>
+                        </li>
+                        <li class="HomeContent__questions-item">
+                            <a class="HomeContent__questions-link" href="#">Will I go to jail for a first time DUI?</a>
+                            In metus velit, placerat sed tristique vel, fringilla vel ex. Aenean vitae lacus arcu. Nunc efficitur ac est quis pulvinar. <a href="#">Read more</a>
+                        </li>
+                        <li class="HomeContent__questions-item">
+                            <a class="HomeContent__questions-link" href="#">What's a DUI surcharge?</a>
+                            In metus velit, placerat sed tristique vel, fringilla vel ex. Aenean vitae lacus arcu. Nunc efficitur ac est quis pulvinar. <a href="#">Read more</a>
+                        </li>
+                    </ul>
+                    <a class="Button +more" href="#">See More FAQs <i class="fa fa-caret-right"></i></a>
+                </div>
+
+                <div class="HomeContent__practices-wrapper">
+                    <h2>Full List of Practice Areas</h2>
+                    <ul class="HomeContent__practices">
+                        <li class="HomeContent__practices-item">
+                            <a class="HomeContent__practices-link" href="#">DUI</a>
+                        </li>
+                        <li class="HomeContent__practices-item">
+                            <a class="HomeContent__practices-link" href="#">Traffic Tickets</a>
+                        </li>
+                        <li class="HomeContent__practices-item">
+                            <a class="HomeContent__practices-link" href="#">Criminal Defense</a>
+                        </li>
+                        <li class="HomeContent__practices-item">
+                            <a class="HomeContent__practices-link" href="#">Personal Injury</a>
+                        </li>
+                        <li class="HomeContent__practices-item">
+                            <a class="HomeContent__practices-link" href="#">General Litigation</a>
+                        </li>
+                        <li class="HomeContent__practices-item">
+                            <a class="HomeContent__practices-link" href="#">Family Law</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeCallToAction__wrapper">
+            <div class="container">
+                <div class="HomeCallToAction">
+                    <ul class="HomeCallToAction__experience">
+                        <li>Put our 30+ years of legal experience to work for YOU.</li>
+                        <li><i class="fa fa-caret-right"></i> <a href="#">Click here</a> to get started!
+                    </ul>
+
+                    <ul class="HomeCallToAction__contact">
+                        <li class="HomeCallToAction__contact-tagline">Free Confidential Review</li>
+                        <li class="HomeCallToAction__contact-number"><i class="fa fa-phone"></i> (803) 256-6276</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="PreFooter__wrapper">
+            <div class="container">
+                <div class="PreFooter">
+                    <ul class="PreFooter__nav">
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">Home</a>
+                        </li>
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">About</a>
+                        </li>
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">Practice Areas</a>
+                        </li>
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">Expectations</a>
+                        </li>
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">Testimonials</a>
+                        </li>
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">Contact Us</a>
+                        </li>
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">Sitemap</a>
+                        </li>
+                        <li class="PreFooter__nav-item">
+                            <a class="PreFooter__nav-link" href="#">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="Footer__wrapper">
+            <div class="container">
+                <div class="Footer">
+                    <ul class="Footer__about">
+                        <li class="m-b-1">Logo</li>
+                        <li class="m-b-1">Copyright &copy;<?php echo date('Y'); ?> Truslow & Truslow Law</li>
+                        <li>Telephone: (803) 256-6276</li>
+                        <li class="m-b-1">Fax: (803) 256-7659</li>
+                        <li>Mailing Address:</li>
+                        <li>PO Box 1465</li>
+                        <li class="m-b-1">Columbia, SC 29202</li>
+                        <li class="m-b-1">Map</li>
+                        <li>914 Richland Street, Suite B-102</li>
+                        <li class="m-b-1">Columbia, SC 29201</li>
+                        <li class="m-b-1"><a href="#">Get Directions</a></li>
+                        <li>Office Hours</li>
+                        <li>Mon-Thur 8:30-5:00</li>
+                        <li>Fri: 8:30-3:00</li>
+                    </ul>
+
+                    <div class="Footer__contact">
+                        <h2>Get in Touch</h2>
+
+                        <form class="Footer__contact-form">
+                            <input class="Footer__contact-input" placeholder="Name" type="text">
+                            <input class="Footer__contact-input" placeholder="Phone" type="text">
+                            <input class="Footer__contact-input" placeholder="Email" type="email">
+                            <textarea class="Footer__contact-input" placeholder="Message" rows="4"></textarea>
+                            <button class="Button +contact" type="submit">Send Email <i class="fa fa-caret-right"></i></button>
+                        </form>
+
+                        <ul class="Footer__social">
+                            <li class="Footer__social-item">
+                                <a class="Footer__social-link" href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li class="Footer__social-item">
+                                <a class="Footer__social-link" href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li class="Footer__social-item">
+                                <a class="Footer__social-link" href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li class="Footer__social-item">
+                                <a class="Footer__social-link" href="#"><i class="fa fa-google-plus"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
