27cfa9ce8b6bedcef892d5002d6a51a1ec0225f3
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-11-03 13:30:16 -0500

    Adds Mazin

--- index.php
+++ index.php
@@ -5,9 +5,305 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <div class="Nav__wrapper">
+            <div class="Nav">
+                <ul class="Nav__contact">
+                    <li class="Nav__contact-item">
+                        <a class="Nav__link --contact" href="#"><i class="fa fa-phone"></i> 800-432-4878</a>
+                    </li>
+                    <li class="Nav__contact-item">
+                        <a class="Nav__link --contact" href="#"><i class="fa fa-envelope"></i> info@mazinlawyers.com</a>
+                    </li>
+                    <li class="Nav__contact-item">
+                        <a class="Nav__link --contact" href="#"><i class="fa fa-comments"></i> Chat Live Now!</a>
+                    </li>
+                </ul>
+
+                <ul class="Nav__social">
+                    <li class="Nav__social-item">
+                        <a class="Nav__link" href="#"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li class="Nav__social-item">
+                        <a class="Nav__link" href="#"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li class="Nav__social-item">
+                        <a class="Nav__link" href="#"><i class="fa fa-google-plus"></i></a>
+                    </li>
+                    <li class="Nav__social-item">
+                        <a class="Nav__link" href="#"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                    <li class="Nav__social-item">
+                        <a class="Nav__link" href="#"><i class="fa fa-youtube"></i></a>
+                    </li>
+                    <li class="Nav__social-item">
+                        <a class="Nav__link" href="#"><i class="fa fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="Header__wrapper">
+            <div class="Header">
+                <ul class="Header__list --left">
+                    <li class="Header__list-item">
+                        <a class="Header__link" href="#">Practice Areas</a>
+                    </li>
+                    <li class="Header__list-item">
+                        <a class="Header__link" href="#">Legal Team</a>
+                    </li>
+                    <li class="Header__list-item">
+                        <a class="Header__link" href="#">Seriously Injured?</a>
+                    </li>
+                </ul>
+                <div class="Header__logo">
+                    <img src="/assets/img/logo.svg" width="100%">
+                </div>
+                <ul class="Header__list --right">
+                    <li class="Header__list-item">
+                        <a class="Header__link" href="#">Locations</a>
+                    </li>
+                    <li class="Header__list-item">
+                        <a class="Header__link" href="#">Testimonials</a>
+                    </li>
+                    <li class="Header__list-item">
+                        <a class="Header__link" href="#">Contact the Firm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="MainGraphic__wrapper">
+            <div class="MainGraphic__inner">
+                <div class="MainGraphic__container">
+                    <div class="MainGraphic">
+                        <ul class="MainGraphic__list">
+                            <li class="MainGraphic__list-item --heading">The Law Firm Other Lawyers Call</li>
+                            <li class="MainGraphic__list-item --text">Our award-winning firm handles all types of personal injury cases throughout Ontario. Contact us today at 800-432-4878 for a 100% FREE confidential consultation.</li>
+                            <li class="MainGraphic__list-item --button">
+                                <a class="Button --primary" href="#">Schedule Your Consultation <i class="fa fa-chevron-right"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="Accolades__wrapper">
+        </div>
+
+        <div class="Content__wrapper">
+            <div class="container">
+                <div class="Content">
+                    <div class="Content__header">
+                        <h1>Toronto Personal Injury Lawyers</h1>
+                    </div>
+                    <div class="MainContent">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id lacus sed enim commodo porta in eu ante. Aenean varius massa eget augue dictum facilisis. Vestibulum non ante rutrum, volutpat nisi eu, pretium arcu. Curabitur consequat, nulla at aliquet vestibulum, massa nunc viverra nisi, posuere mattis nibh purus eu tortor. Pellentesque nec ornare dui, vel feugiat risus. Mauris eu ullamcorper augue. Morbi id nulla eleifend, faucibus leo sed, pharetra purus. Nullam ac erat id eros tincidunt convallis vel eu odio. Mauris semper maximus nunc ut aliquet. Praesent hendrerit euismod mauris, at rutrum neque faucibus ut. Donec et gravida orci.</p>
+
+                        <p>Nulla et turpis et erat placerat iaculis. Duis sit amet justo quis ipsum elementum laoreet non nec tellus. Mauris feugiat dolor nec lectus vulputate, vel fermentum mauris condimentum. Suspendisse sed dolor sem. Vestibulum in dui orci. In efficitur elit accumsan semper feugiat. Suspendisse ac euismod odio. Maecenas ut pellentesque lorem. Vestibulum placerat semper nisi nec congue.</p>
+
+                        <h2>Lorem Ipsum Dolor Sit Amet</h2>
+
+                        <p>Donec dolor leo, pretium nec consequat quis, gravida vitae turpis. Ut laoreet vitae velit at condimentum. Nunc sed mattis lacus. Ut vel diam nisi. Cras lacinia ornare sapien sed commodo. Donec eleifend leo non leo accumsan, vel fringilla nisi malesuada. Aliquam vehicula purus sit amet arcu blandit gravida. Sed interdum neque consectetur dictum finibus.</p>
+                    </div>
+                    <div class="Sidebar">
+                        <div class="SidebarCard">
+                            <div class="SidebarCard__header">
+                                Credentials Count!
+                            </div>
+                            <div class="SidebarCard__body">
+                                <ul class="ListIcon">
+                                    <li class="ListIcon__item --check">
+                                        Lorem Ipsum Dolor Sit Amet 2000 This Keeps Going
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeFeature__wrapper">
+            <div class="container">
+                <div class="HomeFeature">
+                    <div class="HomeFeature__text">
+                        <h2>Helping the Toronto Community</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor augue id vestibulum finibus. Aliquam est tortor, finibus eu lorem bibendum, sollicitudin congue massa. Vestibulum placerat eget enim vel finibus. Nulla eleifend sapien hendrerit purus consequat malesuada.</p>
+                    </div>
+                    <div class="HomeFeature__video">
+                        <div class="Video__wrapper">
+                            <iframe class="Video" width="560" height="315" src="https://www.youtube.com/embed/CUXkGScfPKs" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="Content__wrapper">
+            <div class="container">
+                <div class="Content">
+                    <div class="Sidebar">
+                        <div class="SidebarCard">
+                            <div class="SidebarCard__image">
+                                <img src="/assets/img/SidebarCard__image.jpg">
+                            </div>
+                            <div class="SidebarCard__body --about">
+                                <p class="SidebarCard__body-heading">Gary Mazin, Founder</p>
+
+                                <p>Mauris egestas ultricies dolor, quis commodo purus consequat in. Nulla placerat euismod mi, nec mollis leo. Aenean viverra sodales dui, et scelerisque purus accumsan.</p>
+
+                                <a class="Button --block --primary" href="#">Read More <i class="fa fa-chevron-right"></i></a>
+                            </div>
+                        </div>
+
+                        <div class="Sidebar__box">
+                            <h3>Frequently Asked Questions</h3>
+
+                            <ul class="ListIcon">
+                                <li class="ListIcon__item --chevron">
+                                    <a href="#">Lorem Ipsum Dolor Sit Amet 2000 This Keeps Going</a>
+                                </li>
+                            </ul>
+                            <a class="Button --block --primary" href="#">See More FAQs <i class="fa fa-chevron-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="MainContent">
+                        <h2>Toronto Personal Injury Practice Areas</h2>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id lacus sed enim commodo porta in eu ante. Aenean varius massa eget augue dictum facilisis. Vestibulum non ante rutrum, volutpat nisi eu, pretium arcu. Curabitur consequat, nulla at aliquet vestibulum, massa nunc viverra nisi, posuere mattis nibh purus eu tortor. Pellentesque nec ornare dui, vel feugiat risus. Mauris eu ullamcorper augue. Morbi id nulla eleifend, faucibus leo sed, pharetra purus. Nullam ac erat id eros tincidunt convallis vel eu odio. Mauris semper maximus nunc ut aliquet. Praesent hendrerit euismod mauris, at rutrum neque faucibus ut. Donec et gravida orci.</p>
+
+                        <p>Nulla et turpis et erat placerat iaculis. Duis sit amet justo quis ipsum elementum laoreet non nec tellus. Mauris feugiat dolor nec lectus vulputate, vel fermentum mauris condimentum. Suspendisse sed dolor sem. Vestibulum in dui orci. In efficitur elit accumsan semper feugiat. Suspendisse ac euismod odio. Maecenas ut pellentesque lorem. Vestibulum placerat semper nisi nec congue.</p>
+
+                        <p>Donec dolor leo, pretium nec consequat quis, gravida vitae turpis. Ut laoreet vitae velit at condimentum. Nunc sed mattis lacus. Ut vel diam nisi. Cras lacinia ornare sapien sed commodo. Donec eleifend leo non leo accumsan, vel fringilla nisi malesuada. Aliquam vehicula purus sit amet arcu blandit gravida. Sed interdum neque consectetur dictum finibus.</p>
+
+                        <div class="HomePractices">
+                            <div class="HomePractice">
+                                <a class="Button --home-practice" href="#">Car Accidents <i class="fa fa-chevron-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="Testimonial__wrapper">
+            <div class="container">
+                <div class="Testimonial">
+                    <ul class="Testimonial__list">
+                        <li class="Testimonial__list-item --quote">Aenean cursus risus in ante commodo, ac malesuada lacus rhoncus. Maecenas sed leo posuere, sagittis odio vitae, gravida turpis. Curabitur tellus leo, viverra eget enim porttitor, egestas ultrices augue.</li>
+                        <li class="Testimonial__list-item --source">G. Incognito, Toronto</li>
+                        <li class="Testimonial__list-item">
+                            <a class="Button --primary" href="#">See More Testimonials <i class="fa fa-chevron-right"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="Content__wrapper">
+            <div class="container">
+                <div class="CallToAction">
+                    <div class="CallToAction__text">
+                        <span class="CallToAction__heading">We Don't Get Paid Until You Get Paid!</span>
+                        <p>Mazin & Associates PC enjoys a reputation as Ontario’s top personal injury law firm known for maximum settlements in serious personal injury and accident cases. Our areas of practice include car accidents, motorcycle injuries, brain injuries, spinal cord injuries, slip and falls, wrongful death, product liability, long-term disability and medical malpractice.</p>
+                    </div>
+                    <div class="CallToAction__button">
+                        <a class="Button --block --primary" href="#"><i class="fa fa-mobile-phone"></i> 800-555-4444</a>
+                    </div>
+                    <div class="CallToAction__button">
+                        <a class="Button --block --primary" href="#"><i class="fa fa-comment"></i> Chat Live Now!</a>
+                    </div>
+                    <div class="CallToAction__button">
+                        <a class="Button --block --primary" href="#"><i class="fa fa-envelope"></i> Send Us An Email</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="Map__wrapper">
+            <div class="Map"></div>
+        </div>
+
+        <div class="Content__wrapper">
+            <div class="container">
+                <div class="Content">
+                    <ul class="ListLocation">
+                        <li class="ListLocation__item --heading">Toronto</li>
+                        <li class="ListLocation__item">Tel: 416-250-1234</li>
+                        <li class="ListLocation__item">Toll Free: 1-800-432-HURT</li>
+                        <li class="ListLocation__item">Fax: 416-250-1238</li>
+                        <li class="ListLocation__item">5 Park Home Ave, Suite 120</li>
+                        <li class="ListLocation__item">Toronto, ON M2N 6L4</li>
+                    </ul>
+                    <ul class="ListLocation">
+                        <li class="ListLocation__item --heading">Kitchener</li>
+                        <li class="ListLocation__item">Toll Free: 1-800-432-HURT</li>
+                        <li class="ListLocation__item">Fax: 416-250-1238</li>
+                        <li class="ListLocation__item">55 King Street West, Suite 700</li>
+                        <li class="ListLocation__item">Kitchener, ON N2G 4W1</li>
+                    </ul>
+                    <ul class="ListLocation">
+                        <li class="ListLocation__item --heading">Belleville</li>
+                        <li class="ListLocation__item">Toll Free: 1-800-432-HURT</li>
+                        <li class="ListLocation__item">Fax: 416-250-1238</li>
+                        <li class="ListLocation__item">147 Front St.</li>
+                        <li class="ListLocation__item">Belleville, ON K8N 2Y7</li>
+                    </ul>
+                    <ul class="ListLocation">
+                        <li class="ListLocation__item --heading">Hamilton</li>
+                        <li class="ListLocation__item">Toll Free: 1-800-432-HURT</li>
+                        <li class="ListLocation__item">Fax: 416-250-1238</li>
+                        <li class="ListLocation__item">290 Caroline St South</li>
+                        <li class="ListLocation__item">Hamilton, ON L8P 3L9</li>
+                    </ul>
+                    <ul class="ListLocation">
+                        <li class="ListLocation__item --heading">Guelph</li>
+                        <li class="ListLocation__item">Toll Free: 1-800-432-HURT</li>
+                        <li class="ListLocation__item">Fax: 416-250-1238</li>
+                        <li class="ListLocation__item">9-485 Silvercreek Pkwy North</li>
+                        <li class="ListLocation__item">Guelph, ON N1H 7K5</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="Footer__wrapper">
+            <div class="container">
+                <div class="Footer">
+                    <div class="Footer__text">
+                        <ul class="Footer__list">
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Practice Areas</a>
+                            </li>
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Practice Areas</a>
+                            </li>
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Practice Areas</a>
+                            </li>
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Practice Areas</a>
+                            </li>
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Practice Areas</a>
+                            </li>
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Practice Areas</a>
+                            </li>
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Practice Areas</a>
+                            </li>
+                        </ul>
+
+                        <p>Copyright &copy;<?php echo date('Y'); ?>, Mazin & Associates PC. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
