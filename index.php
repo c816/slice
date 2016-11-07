648718b1d31f72eec98ca206336be7624f850b63
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-11-07 12:21:34 -0600

    Adds Mokaram

--- index.php
+++ index.php
@@ -5,9 +5,207 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+        <script src="https://use.typekit.net/tgw4xim.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <div class="MainGraphic__wrapper">
+        </div>
+
+        <div class="Accolades__wrapper">
+            <div class="Accolades">
+                <img src="/assets/img/accolades/nlp.png">
+                <img src="/assets/img/accolades/ngh.png">
+                <img src="/assets/img/accolades/icbch.png">
+                <img src="assets/img/accolades/aha.png">
+                <img src="assets/img/accolades/hmi.png">
+            </div>
+        </div>
+
+        <div class="HomeContent__wrapper">
+            <div class="container">
+                <div class="HomeContent">
+                    <div class="HomeContent__header">
+                        <h1>Welcome to the Mokaram Law Firm</h1>
+                    </div>
+                    <div class="HomeContent__text">
+                        <p>Donec accumsan sollicitudin commodo. Cras semper tristique mauris quis pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas risus tellus, volutpat ut fermentum in, consectetur id nisi. Phasellus at ligula ipsum. Pellentesque et porttitor eros, cursus accumsan nulla. Ut dignissim leo et velit luctus, quis fermentum lectus gravida. Nunc imperdiet efficitur massa, nec dapibus quam. Phasellus a ultrices metus. Nullam consequat ornare turpis, consectetur commodo mauris mollis vitae. Maecenas eu blandit lacus. Vivamus id turpis ut est cursus euismod at et odio. Ut id lorem ac elit tincidunt placerat. Vivamus eget viverra nisl. In hac habitasse platea dictumst.</p>
+
+                        <p>Integer ut auctor libero, ut laoreet odio. Sed ornare sed massa ac interdum. Suspendisse sed ex sed est luctus suscipit. Nunc pharetra velit eu urna cursus imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque auctor, urna ac tincidunt porttitor, ipsum enim cursus urna, nec dictum neque dui sit amet urna. Donec mi nisi, sollicitudin sed commodo dapibus, iaculis sed arcu. Nunc lobortis urna nec turpis euismod sagittis. Vivamus auctor, lacus eget fringilla consequat, est est iaculis ipsum, at vestibulum sem risus et mauris. Nullam at felis at nisl sagittis hendrerit vel lobortis purus. Etiam et ipsum tellus. Vivamus eget arcu eros. Integer a scelerisque dui. Vestibulum sit amet blandit mi.</p>
+
+                        <p>Praesent ac vehicula metus. Aenean et massa lorem. Maecenas a porta massa, sit amet scelerisque sem. Maecenas nec urna id urna condimentum porta. Nullam quis nunc odio. Sed convallis tincidunt eros, finibus lacinia mauris feugiat ut. Cras accumsan, arcu vel laoreet accumsan, dolor magna aliquet metus, in pharetra eros felis id lorem. Maecenas lobortis, tellus vitae tempus lacinia, arcu lacus venenatis enim, eget sagittis sapien arcu id lectus. Nunc porta eros in fringilla porta.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeAttorneys__wrapper">
+            <div class="container">
+                <div class="HomeAttorneys">
+                    <div class="HomeAttorneys__header">
+                        <h2>Meet Our Attorneys</h2>
+                    </div>
+                    <div class="HomeAttorneys__card offset-md-1">
+                        <div class="Card">
+                            <div class="Card__image">
+                                <img src="http://placehold.it/480x480/000000/ffffff">
+                            </div>
+                            <div class="Card__body">
+                                <ul class="Card__list">
+                                    <li class="Card__list-item --attorney-name">Ali Mokaram</li>
+                                    <li class="Card__list-item --attorney-role">Managing Partner</li>
+                                    <li class="Card__list-item --attorney-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor hendrerit suscipit. Aliquam tellus quam, aliquam eu feugiat ac, pharetra id massa. Sed et est et nunc aliquet cursus. <a href="#">READ MORE</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="HomeAttorneys__card offset-md-2">
+                        <div class="Card">
+                            <div class="Card__image">
+                                <img src="http://placehold.it/480x480/000000/ffffff">
+                            </div>
+                            <div class="Card__body">
+                                <ul class="Card__list">
+                                    <li class="Card__list-item --attorney-name">Peyman Momeni</li>
+                                    <li class="Card__list-item --attorney-role">Attorney</li>
+                                    <li class="Card__list-item --attorney-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor hendrerit suscipit. Aliquam tellus quam, aliquam eu feugiat ac, pharetra id massa. Sed et est et nunc aliquet cursus. <a href="#">READ MORE</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeContent__wrapper">
+            <div class="container">
+                <div class="HomeContent">
+                    <div class="HomeContent__header">
+                        <h2>Practice Areas</h2>
+                    </div>
+                    <div class="PracticeArea__wrapper">
+                        <div class="PracticeArea">
+                            <div class="PracticeArea__image">
+                                <img src="http://placehold.it/320x320">
+                            </div>
+                            <div class="PracticeArea__text">
+                                <ul class="PracticeArea__list">
+                                    <li class="PracticeArea__list-item --title">
+                                        <h3>NFL Concussions</h3>
+                                    </li>
+                                    <li class="PracticeArea__list-item --description">
+                                        Praesent ac vehicula metus. Aenean et massa lorem. Maecenas a porta massa, sit amet scelerisque sem. Maecenas nec urna id urna condimentum porta. Nullam quis nunc odio. <a href="#">READ MORE</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="PracticeArea__wrapper">
+                        <div class="PracticeArea">
+                            <div class="PracticeArea__image">
+                                <img src="http://placehold.it/320x320">
+                            </div>
+                            <div class="PracticeArea__text">
+                                <ul class="PracticeArea__list">
+                                    <li class="PracticeArea__list-item --title">
+                                        <h3>Personal Injury</h3>
+                                    </li>
+                                    <li class="PracticeArea__list-item --description">
+                                        Praesent ac vehicula metus. Aenean et massa lorem. Maecenas a porta massa, sit amet scelerisque sem. Maecenas nec urna id urna condimentum porta. Nullam quis nunc odio. <a href="#">READ MORE</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="HomeFeature__wrapper">
+        </div>
+
+        <div class="HomeContent__wrapper">
+            <div class="container">
+                <div class="HomeContent">
+                    <div class="HomeContent__half">
+                        <h2 class="text-xs-center">Records of Success</h2>
+                        <ul class="ListIcon">
+                            <li class="ListIcon__item --certificate">$100 Million for lorem ipsum dolot sit amet and so on and so forth.</li>
+                        </ul>
+                    </div>
+                    <div class="HomeContent__half">
+                        <h2 class="text-xs-center">In the Media</h2>
+                        <p>Praesent ac vehicula metus. Aenean et massa lorem. Maecenas a porta massa, sit amet scelerisque sem. Maecenas nec urna id urna condimentum porta. Nullam quis nunc odio.</p>
+                        <div class="Video__wrapper">
+                            <iframe class="Video" width="560" height="315" src="https://www.youtube.com/embed/CUXkGScfPKs" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="CallToAction__wrapper">
+            <div class="CallToAction">
+                <div class="CallToAction__text">
+                    Ready to get started? Call 713-952-4445 Today!
+                </div>
+            </div>
+        </div>
+
+        <div class="Footer__wrapper">
+            <div class="container">
+                <div class="Footer">
+                    <div class="Footer__column">
+                        <ul class="Footer__list">
+                            <li class="Footer__list-item --firm-name">Mokaram Law Firm</li>
+                            <li class="Footer__list-item">2500 West Loop S</li>
+                            <li class="Footer__list-item">Suite #450</li>
+                            <li class="Footer__list-item">Houston, TX 77027</li>
+                            <li class="Footer__list-item">Phone: 713-952-4445</li>
+                            <li class="Footer__list-item mb-1">Fax: 713-555-5555</li>
+                            <li class="Footer__list-item">
+                                <a class="Footer__link" href="#">Map & Directions</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="Footer__column">
+                        <h3>Quick Links</h3>
+                    </div>
+                    <div class="Footer__column">
+                        <h3>Send Us an Email</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="SubFooter__wrapper">
+            <div class="container">
+                <div class="SubFooter">
+                    <div class="SubFooter__copyright">
+                        Copyright &copy;<?php echo date('Y'); ?> Mokaram Law Firm. All rights reserved.
+                    </div>
+                    <div class="SubFooter__list">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item">
+                                <a class="SubFooter__link" href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="SubFooter__link" href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="SubFooter__link" href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="SubFooter__link" href="#"><i class="fa fa-youtube"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
